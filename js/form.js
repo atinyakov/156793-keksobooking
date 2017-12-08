a0d418316064a5c8a263c10e2d7892060a141dc4
atinyakov <akoff16@gmail.com>
2017-12-08 20:46:04 +0300

    encapsulation

---
+++ js/form.js
@@ -0,0 +1,66 @@
+'use strict';
+(function () {
+  var mouseAction = window.map.mapContainer.querySelector('.map__pin--main');
+  var startForm = function () {
+    window.map.form.classList.remove('notice__form--disabled');
+    window.map.mapContainer.classList.remove('map--faded');
+    window.pin.createFragment();
+    for (var k = 0; k < window.map.fieldset.length; k++) {
+      window.map.fieldset[k].removeAttribute('disabled', 'disabled');
+    }
+  };
+
+  mouseAction.addEventListener('mouseup', startForm);
+  mouseAction.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.data.ENTER_KEYCODE) {
+      startForm();
+    }
+  });
+  // form validation
+  var userSelectCheckIn = window.map.form.querySelector('#timein');
+  var userSelectCheckout = window.map.form.querySelector('#timeout');
+  // syncs checkin with checkout
+  userSelectCheckIn.addEventListener('change', function () {
+    userSelectCheckout.value = userSelectCheckIn.value;
+  });
+  userSelectCheckout.addEventListener('change', function () {
+    userSelectCheckIn.value = userSelectCheckout.value;
+  });
+
+  var typeToMinPrice = {
+    bungalo: 0,
+    flat: 1000,
+    house: 5000,
+    palace: 10000
+  };
+  // syncs type with price
+  var userSelectType = window.map.form.querySelector('#type');
+  var userPrice = window.map.form.querySelector('#price');
+  userSelectType.addEventListener('change', function () {
+    for (var userType in typeToMinPrice) {
+      if (userType === userSelectType.value) {
+        userPrice.min = typeToMinPrice[userType];
+      }
+    }
+  });
+
+  var roomNumber = window.map.form.querySelector('#room_number');
+  var capacity = window.map.form.querySelector('#capacity');
+  // var options = selectGuests.querySelectorAll('option');
+
+  var ROOMS_CAPACITY = {
+    '1': ['1'],
+    '2': ['2', '1'],
+    '3': ['3', '2', '1'],
+    '100': ['0']
+  };
+  var roomNumberChangeHandler = function () {
+    if (capacity.options.length > 0) {
+      [].forEach.call(capacity.options, function (item) {
+        item.selected = (ROOMS_CAPACITY[roomNumber.value][0] === item.value) ? true : false;
+        item.hidden = (ROOMS_CAPACITY[roomNumber.value].indexOf(item.value) >= 0) ? false : true;
+      });
+    }
+  };
+  roomNumber.addEventListener('change', roomNumberChangeHandler);
+})();
